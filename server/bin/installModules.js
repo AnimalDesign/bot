cbe614f9c7b670d7604b61d124340434398ac546
Matthias Esterl <me@madcity.at>
2016-01-13 18:35:18 +0100

    Minor changes

--- server/bin/installModules.js
+++ server/bin/installModules.js
@@ -1,15 +1,15 @@
-var fs = require('fs'),
+var filesystem = require('fs'),
 	resolve = require('path').resolve,
 	join = require('path').join,
 	cp = require('child_process');
 
 var lib = resolve(__dirname, '../lib/modules/')
-fs.readdirSync(lib)
+filesystem.readdirSync(lib)
 	.forEach(function(mod) {
 		var path = join(lib, mod);
 
 		// Ensure path is a module and has a package.json
-		if (!fs.existsSync(join(path, 'package.json'))) {
+		if (!filesystem.existsSync(join(path, 'package.json'))) {
 			return;
 		}
 

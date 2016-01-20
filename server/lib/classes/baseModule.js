14e66d3ab60ed8b0b24ddbafd4efc88c23a21903
Matthias Esterl <me@madcity.at>
2016-01-20 01:38:54 +0100

    Cleanup of baseModule class

--- server/lib/classes/baseModule.js
+++ server/lib/classes/baseModule.js
@@ -3,22 +3,24 @@
 var filesystem = require('fs'),
 	path = require('path');
 
+/**
+ * Base class for modules
+ * @class baseModule
+ */
 class baseModule {
 	constructor() {
 		this.moduleName = this.constructor.name;
 		this.config = {};
 
-		var configPath = path.resolve(__dirname, '../modules/' + this.moduleName + '/config.json');
-		if (filesystem.existsSync(configPath)) {
-			try {
-				this.config = JSON.parse(
-					filesystem.readFileSync(configPath)
-				);
-			} catch (e) {
-				console.log('Error loading config.json', {moduleName: this.moduleName});
-			}
+		try {
+			this.config = JSON.parse(
+				filesystem.readFileSync(path.resolve(process.cwd(), 'server/modules', this.moduleName, 'config.json'))
+			);
+		} catch (e) {
+			console.log('Error loading config.json', {
+				moduleName: this.moduleName
+			});
 		}
-
 	}
 
 	init() {}

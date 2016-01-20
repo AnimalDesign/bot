49807713869fd557457fcc66eaaefa5fcf77005f
Matthias Esterl <me@madcity.at>
2016-01-20 17:28:40 +0100

    Rewrote moduleLoader to use ES6 class

--- server/lib/moduleLoader.js
+++ server/lib/moduleLoader.js
@@ -1,15 +1,25 @@
 'use strict';
 
 var filesystem = require('fs'),
+	util = require('util'),
+	schedule = require('node-schedule'),
+
 	modules = {},
 	actions = {},
 	tasks = {},
 	events = {},
-	commands = {};
+	commands = [];
 
-var moduleLoader = function moduleLoader() {
-
-	this.loadModules = function() {
+/**
+ * Autoloader for ANIMAL bot modules
+ * @class moduleLoader
+ */
+class moduleLoader {
+	/**
+	 * Load all modules
+	 * @returns {object}
+	 */
+	loadModules() {
 		var path = 'server/lib/modules/',
 			self = this;
 
@@ -18,7 +28,8 @@ var moduleLoader = function moduleLoader() {
 				return;
 			}
 
-			modules[moduleName] = require('./modules/' + moduleName + '/module');
+			var moduleClass = require('./modules/' + moduleName + '/module');
+			modules[moduleName] = new moduleClass();
 			modules[moduleName].init();
 		});
 
@@ -28,32 +39,119 @@ var moduleLoader = function moduleLoader() {
 
 	/**
 	 * Registers an action
+	 * @param {object} action
+	 * @param {string} action.moduleName
+	 * @param {string} action.name
+	 * @param {(string|function)} action.matcher
+	 * @param {(string|function)} action.callback
 	 */
-	this.registerAction = function(moduleName, action) {
+	registerAction(action) {
+		if (!util.isFunction(action.callback)) {
+			action.callback = modules[action.moduleName][action.callback];
+		}
+
+		if (!util.isFunction(action.matcher)) {
+			action.matcher = modules[action.moduleName][action.matcher];
+		}
+
+		actions[action.moduleName + '.' + action.name] = action;
+	};
+
+	/**
+	 * Returns all registered actions
+	 * @returns {object}
+	 */
+	getActions() {
+		return actions;
+	}
+
+	/**
+	 * Returns callbacks of matching actions
+	 * @param {string} exp
+	 * @returns {array}
+	 */
+	matchActions(exp) {
+		var callbacks = [];
+
+		for (var actionsName in actions) {
+			if (actions[actionsName].matcher(exp)) {
+				callbacks.push(actions[actionsName].callback(exp));
+			}
+		}
+
+		return callbacks;
+	}
+
+	/**
+	 * Register a new task
+	 * @param {object} task
+	 * @param {string} task.moduleName
+	 * @param {string} task.name
+	 * @param {*} task.interval
+	 * @param {(string|function)} task.callback
+	 */
+	registerTask(task) {
+		if (global.isCommandline) {
+			return;
+		}
+
+		if (!util.isFunction(task.callback)) {
+			task.callback = modules[task.moduleName][task.callback];
+		}
+
+		task._job = schedule.scheduleJob(task.interval, task.callback);
+		tasks[task.moduleName + '.' + task.name] = task;
+	};
+
+	/**
+	 * Returns all registered tasks
+	 * @returns {object}
+	 */
+	getTasks() {
+		return tasks;
+	}
+
+	/**
+	 * Registers an event
+	 * @param {object} event
+	 */
+	registerEvent(event) {
 
 	};
 
 	/**
-	 * Registers a recurring task
+	 * Returns all registered events
+	 * @returns {object}
 	 */
-	this.registerTask = function(moduleName, task) {
+	getEvents() {
+		return events;
+	}
 
+	/**
+	 * Registers a command
+	 * @param {object} command
+	 * @param {string} command.moduleName
+	 * @param {string} command.name
+	 * @param {string} [command.description]
+	 * @param {array} [command.attributes]
+	 * @param {array} [command.options]
+	 * @param {(string|function)} command.callback
+	 */
+	registerCommand(command) {
+		if (!global.isCommandline) {
+			return;
+		}
+
+		commands.push(command);
 	};
 
 	/**
-	 * Registers event
+	 * Returns all registed commands
+	 * @returns {array}
 	 */
-	this.registerEvent = function(moduleName, event) {
-
-	};
-
-	/**
-	 * Registers command
-	 * @private
-	 */
-	this.registerCommand = function(moduleName, command) {
-
-	};
+	getCommands() {
+		return commands;
+	}
 }
 
 module.exports = exports = new moduleLoader();
